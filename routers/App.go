b8d5a92f0cc4f5bc8f718e17cda68afeb72d87a0
GaoJH <779697959@qq.com>
2022-12-31 19:41:07 +0800

    AddSwagger

--- routers/App.go
+++ routers/App.go
@@ -2,11 +2,16 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"go-chat/controllers"
+	"go-chat/docs"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "" //TODO:?
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.GET("/test", controllers.TestController)
 	return r
 }
